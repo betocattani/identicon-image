2b51e022c2bb4a9d1f3e399d794f9130da801da0
Cattani <contato.cattani@gmail.com>
2020-07-02 00:56:11 -0300

    create hash_string method to encode string to md5

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,2 +1,13 @@
 defmodule Identicon do
+  def main(input) do
+    input
+    |> hash_string
+  end
+
+  def hash_string(input)  do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
 end
